69abb62a32f599e70ec9e49e36191e5d60270e00
Eby JK <ebyjkavungal@gmail.com>
2026-07-06 23:02:06 +0530

    updated app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,8 @@ const app = express();
 // Middleware
 app.use(cors({
 
-        origin: "http://localhost:5173",
+        origin:[ "http://localhost:5173",
+      "https://child-health-monitoring-dashboard-gamma.vercel.app",],
 
 }));
 app.use(express.json());
